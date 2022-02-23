4f3969922e4f832889c7218b95c591bb7001ba67
AngelGomez <AngelAntonioGomezNicolas@bitbucket.org>
2022-02-23 16:47:53 +0100

    aloha
    aloha

--- app.js
+++ app.js
@@ -6,24 +6,23 @@ function Tokenizer(){
 
 }
 
-
+//aloha
 function testTokenizer(){
-  
+
   var t = ........;
   var countA = 0;
   var testString = ['H','o','l','a',' ','c','o','m',' ','a','n','e','u','?'];
 
 
   t.on('a', .....);
-  
+
   t.onDefault(......);
 
   //here goes the code to run the test over testString
-  
+
   console.log("numero de a's: " + countA);
   console.log("numero de c's: " + .............);
   console.log("numero d'altres caracters: " + ................);
 }
 
 testTokenizer();
-
